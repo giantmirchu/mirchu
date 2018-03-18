ce26868a1b2aac03a3f9c430284c53284d38a183
Zullu <giantmirchu@gmail.com>
2018-03-18 14:11:19 +0500

    adding index file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>Mirchu</title>
+    </head>
+    <body>
+    <h2>Welcome To giant's Site</h2>
+    </body>
+</html>
